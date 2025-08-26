e649410ac928bd7a829db1b70ccd26fabe815a9e
PAVONIS9999 <1josephlmg@gmail.com>
2025-08-26 19:36:51 +0200

    Added Position class, properties for x and y

---
+++ Position.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_ADVENTURE_GAME
+{
+    internal class Position
+    {
+        //Private Field Declaration for x and y coordinates
+        private int xcord;
+        private int ycord;
+
+        public Position(int X, int Y) //constructor with 2 parameters
+        {
+            xcord = X;
+            ycord = Y;
+
+        }
+        //properties for x and y coordinates
+
+        public int Xcord
+        { get; set; }
+
+        public int Ycord
+        { get; set; }
+
+    }
+}
