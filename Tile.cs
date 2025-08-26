530ab04e9c808eb53af803d16a346dd578933622
PAVONIS9999 <1josephlmg@gmail.com>
2025-08-26 19:59:17 +0200

    Added Tile class with fields,properties and constructors of type Position

---
+++ Tile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_ADVENTURE_GAME
+{
+    abstract class Tile
+    {
+        private Position position; //private field of type position
+        
+            //properties to expose x and y values
+        public Position x
+        {
+            get { return position; }         
+                
+        }
+
+        public Position y
+        {
+            get { return position; }
+
+        }
+
+        public Tile(Position P) //constructor that accepts a position parameter type
+        {
+            position = P;
+
+        }
+    }
+}
